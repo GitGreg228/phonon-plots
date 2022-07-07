29e0cb49c05ecb1c8fe167237d773a9eb6f9f13f
GitGreg228 <shutov.gm@phystech.edu>
2022-07-08 01:10:44 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,10 +2,12 @@ from pymatgen.core.structure import Structure
 from pymatgen.symmetry.kpath import KPathSeek, KPathBase, KPathSetyawanCurtarolo
 from pymatgen.phonon.bandstructure import PhononBandStructureSymmLine
 from pymatgen.phonon.plotter import PhononBSPlotter
+from pymatgen.phonon.dos import CompletePhononDos
 from pymatgen.io.phonopy import get_ph_bs_symm_line_from_dict
 from tqdm import tqdm
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import numpy as np
 import json
 import argparse
@@ -24,12 +26,16 @@ def main():
 
     with open(os.path.join(args.path, 'band.yaml'), 'r') as f:
         band = yaml.load(f, Loader=yaml.FullLoader)
+
+    dos = np.loadtxt(os.path.join(args.path, 'total_dos.dat'), unpack=True)
+
     bs = get_ph_bs_symm_line_from_dict(band, labels_dict=qpoints['kpoints'])
     plotter = PhononBSPlotter(bs)
     data = plotter.bs_plot_data()
 
     new_ticks = dict()
     new_ticks['distance'] = list()
+    new_ticks['sep_distance'] = list()
     new_ticks['label'] = list()
     for i, dist in enumerate(data['ticks']['distance']):
         _dist = np.round(dist, 5)
@@ -40,6 +46,7 @@ def main():
                 new_label = r'$\Gamma$'
             else:
                 if r'\mid' in old_label:
+                    new_ticks['sep_distance'].append(_dist)
                     split = old_label.split(r'\mid')
                     sep = list()
                     for qpoint in split:
@@ -52,23 +59,42 @@ def main():
                 else:
                     new_label = r'$\mathrm{' + old_label + r'}$'
             new_ticks['label'].append(new_label)
+    new_ticks['label'][-1] = new_ticks['label'][-1] + r'$\mid 0$'
 
     font = {'weight': 'normal',
-            'size': 15}
+            'size': 20}
     mpl.rc('font', **font)
 
+    fig = plt.figure(figsize=(12, 6))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1])
+    gs.update(wspace=0, hspace=0)
+    ax0 = plt.subplot(gs[0])
+    # fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 1]}, figsize=(12, 6))
     print('Plotting phonon bands ...')
-    plt.figure(figsize=(8, 6))
     for i in tqdm(range(len(data['distances']))):
         for j, freq in enumerate(data['frequency'][i]):
-            plt.plot(data['distances'][i], freq, color='steelblue', zorder=0)
-    plt.xticks(new_ticks['distance'], labels=new_ticks['label'])
-    plt.autoscale(enable=True, axis='x', tight=True)
+            ax0.plot(data['distances'][i], freq, color='steelblue', zorder=0, linewidth=1)
+    ax0.set_xticks(new_ticks['distance'])
+    ax0.set_xticklabels(new_ticks['label'])
+    ax0.autoscale(enable=True, axis='x', tight=True)
     ylim = np.max(data['frequency']) * 1.05
-    plt.vlines(new_ticks['distance'], 0, ylim, color='k', zorder=1)
-    plt.ylim(0, ylim)
-    plt.ylabel('Frequency, THz')
-    plt.savefig(os.path.join(args.path, 'band.pdf'))
+    ax0.vlines(new_ticks['distance'], 0, ylim, color='k', linestyle='--', zorder=1, linewidth=1)
+    ax0.vlines(new_ticks['sep_distance'], 0, ylim, color='k', zorder=2, linewidth=2)
+    ax0.set_ylim(0, ylim)
+    ax0.set_ylabel('Frequency, THz')
+
+    ax1 = plt.subplot(gs[1], sharey=ax0)
+    # plt.subplots_adjust(wspace=0, hspace=0)
+    # plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax1.get_yticklabels(), visible=False)
+
+    ax1.fill(dos[1], dos[0], dos[1], np.zeros(dos[1].shape), facecolor='lightsteelblue', edgecolor='steelblue')
+    ticks = np.array([np.round(np.max(dos[1])/2), 2*np.round(np.max(dos[1])/2)])
+    ax1.set_xticks(ticks)
+    ax1.set_xlim(0)
+    ax1.set_xlabel('Phonon DOS')
+    fig.tight_layout()
+    fig.savefig(os.path.join(args.path, 'band.pdf'))
 
 
 if __name__ == '__main__':
